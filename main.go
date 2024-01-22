a7c67d52dd5e558741ba43cf11feedc62abf8d2c
jeff <jroutledge7@gmail.com>
2024-01-22 00:11:32 -0700

    removed unused code

--- main.go
+++ main.go
@@ -6,15 +6,7 @@ import (
 	"github.com/jeffroutledge/CliPokedex/internal/pokeapi"
 )
 
-var locationUrls []string
-var locationUrlIndex int
-
-// var loactions []PokeLocations
-
 func main() {
-	locationUrls = append(locationUrls, "https://pokeapi.co/api/v2/location/")
-	locationUrlIndex = 0
-
 	pokeClient := pokeapi.NewClient(5 * time.Second)
 	cfg := &config{
 		pokeapiClient: pokeClient,
